0eabc1fbffff0ec8ab2ccd08db8526f827b981f9
EvgeniiaPopova <evgpopova3@gmail.com>
2017-04-07 18:40:17 +0300

    New file index2.php for parse XmlResponse by XMLReader

---
+++ index2.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by:
+ * User: Zhenia Popova
+ * E-mail: zhenia@avaito.com
+ * Date: 07.04.17
+ */
+
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE);
+
+try {
+    $context = stream_context_create(array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    ));
+
+    $options = array(
+        'exceptions' => 1,
+        'trace' => 1,
+        'encoding' => 'UTF-8',
+        'connection_timeout' => 30,
+        'stream_context' => $context,
+        'location' => 'https://83.218.157.188:443/test/khaosids.exe/soap/IKosWeb',
+        'uri' => 'http://tempuri.org/'
+    );
+    $client = new SoapClient('https://83.218.157.188:443/test/khaosids.exe/wsdl/IKosWeb?wsdl', $options);
+    $responseXML = $client->ExportOrderStatus();
+
+    $reader = new XMLReader();
+    $reader->XML($responseXML, NULL, 0);
+    while ($reader->read()) {
+        if ($reader->nodeType == XMLReader::ELEMENT) {
+            if ($reader->localName == 'ORDER') {
+                $data = array();
+                $data['id'] = $reader->getAttribute('ID');
+                $data['ref'] = $reader->getAttribute('REF');
+                $data['urn'] = $reader->getAttribute('URN');
+
+                $reader->read();
+                if ($reader->nodeType == XMLReader::TEXT) {
+                    $data['value'] = $reader->value;
+                    $reader->read();
+                print_r($data);
+                }
+            }
+        }
+    }
+
+//    print_r ($responseXML);
+} catch (Exception $e) {
+    print $e->getMessage();
+}
