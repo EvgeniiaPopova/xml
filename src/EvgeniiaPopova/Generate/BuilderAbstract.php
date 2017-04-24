cb2f58ce5cd4c738acef15782d0aec7468501b9a
EvgeniiaPopova <zhenia@avaito.com>
2017-04-24 18:08:51 +0300

    +ToDo

--- src/EvgeniiaPopova/Generate/BuilderAbstract.php
+++ src/EvgeniiaPopova/Generate/BuilderAbstract.php
@@ -10,22 +10,36 @@
 namespace Generate;
 
 /**
- * @TODO Why here was plus if var $dom hasn't PHPDoc????? Let alone other methods????
+ * @TODO Why here was plus if var $dom hasn't PHPDoc????? Let alone other methods???? +?
  *
  * Class BuilderAbstract
  * @package Generate
  */
 abstract class BuilderAbstract
 {
+    /**
+     * @var \DOMDocument|string
+     */
     protected $dom = '';
 
+    /**
+     * @param $name
+     * @param \ArrayObject $dataObj
+     * @return mixed
+     */
     abstract public function buildXml($name, \ArrayObject $dataObj);
 
+    /**
+     * BuilderAbstract constructor.
+     */
     public function __construct()
     {
         $this->dom = new \DOMDocument('1.0');
     }
 
+    /**
+     * @return \DOMDocument|string
+     */
     protected function getDom()
     {
         if (!isset($this->dom)) {
@@ -34,6 +48,10 @@ abstract class BuilderAbstract
         return $this->dom;
     }
 
+    /**
+     * @param string $name
+     * @return Xml\
+     */
     public function save($name)
     {
         $dom = $this->dom;
